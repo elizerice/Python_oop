a3312bc8c3e8f200b14e3c0b9ee3c6b02326c0d8
elizerice <114720693+elizerice@users.noreply.github.com>
2024-04-23 23:10:33 +0500

    Create 03.py

---
+++ 02/03.py
@@ -0,0 +1,55 @@
+class AmericanDate:
+    def __init__(self, year, month, day):
+        self.year = year
+        self.month = month
+        self.day = day
+
+    def set_year(self, year):
+        self.year = year
+
+    def set_month(self, month):
+        self.month = month
+
+    def set_day(self, day):
+        self.day = day
+
+    def get_year(self):
+        return self.year
+
+    def get_month(self):
+        return self.month
+
+    def get_day(self):
+        return self.day
+
+    def format(self):
+        return "{:02}.{:02}.{}".format(self.month, self.day, self.year)
+
+
+class EuropeanDate:
+    def __init__(self, year, month, day):
+        self.year = year
+        self.month = month
+        self.day = day
+
+    def set_year(self, year):
+        self.year = year
+
+    def set_month(self, month):
+        self.month = month
+
+    def set_day(self, day):
+        self.day = day
+
+    def get_year(self):
+        return self.year
+
+    def get_month(self):
+        return self.month
+
+    def get_day(self):
+        return self.day
+
+    def format(self):
+        return "{:02}.{:02}.{}".format(self.day, self.month, self.year)
+
