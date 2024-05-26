d342da1b13e4ffa7bfe281f5a5f8d9dd6a420579
elizerice <114720693+elizerice@users.noreply.github.com>
2024-05-26 17:27:49 +0500

    Create 02.py

---
+++ 04/02.py
@@ -0,0 +1,26 @@
+class Organismus:
+    pass
+
+class Acellularia(Organismus):
+    pass
+
+class Cellularia(Organismus):
+    pass
+
+class Prokaryota(Cellularia):
+    pass
+
+class Eukaryota(Cellularia):
+    pass
+
+class Unicellularia(Eukaryota):
+    pass
+
+class Fungi(Eukaryota):
+    pass
+
+class Plantae(Eukaryota):
+    pass
+
+class Animalia(Eukaryota):
+    pass
