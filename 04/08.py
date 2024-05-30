fbe153feb38c7e5f6dc7e0fe47c7f80316a30a92
elizerice <114720693+elizerice@users.noreply.github.com>
2024-05-30 23:29:44 +0500

    Update 08.py

--- 04/08.py
+++ 04/08.py
@@ -1,26 +1,25 @@
 class Summator:
     def __init__(self, n):
         self.n = n
-        self.num = 1
+        self.degree = 1
 
-  
+    
     def transform(self):
         self.n = self.n
 
-  
+    
     def sum(self):
         res = 0
         for i in range(self.n + 1):
-            res += i ** self.num
+            res += i ** self.degree
         return res
 
 
 class SquareSummator(Summator):
     def transform(self):
-        self.num = 2
+        self.degree = 2
 
 
 class CubeSummator(Summator):
     def transform(self):
-        self.num = 3
-
+        self.degree = 3
