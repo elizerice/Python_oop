117a87c347f66331e8cdfd6d125ee0de8d6b587c
elizerice <114720693+elizerice@users.noreply.github.com>
2024-05-31 16:40:55 +0500

    Create 04.py

---
+++ 05/04.py
@@ -0,0 +1,10 @@
+def cached(func):
+    cache = {}
+
+    def wrapper(*args):
+        if args in cache:
+            return cache[args]
+        result = func(*args)
+        cache[args] = result
+        return result
+    return wrapper
